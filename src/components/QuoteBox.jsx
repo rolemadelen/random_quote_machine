9d6b0618def9cf6c5eac2f7e0d37a0ef455f555d
Madelen <rolemadelen@pm.me>
2022-09-11 17:48:22 +0900

    一応終わり | Demo added

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
 import "../assets/styles/QuoteBox.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowsRotate, faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 
 class QuoteBox extends React.Component {
   constructor(props) {
@@ -15,38 +17,64 @@ class QuoteBox extends React.Component {
   }
 
   componentDidMount() {
-    this.populateNewQuote();
+    const quoteURL = "https://api.quotable.io/random";
+
+    axios.get(quoteURL).then((response) => {
+      this.setState({
+        text: response.data.content,
+        author: response.data.author,
+      });
+    });
   }
 
   populateNewQuote() {
-    // const quoteURL = "https://api.quotable.io/random";
+    const quoteURL = "https://api.quotable.io/random";
 
-    // axios.get(quoteURL).then((response) => {
-    //   this.setState({
-    //     text: response.data.content,
-    //     author: response.data.author,
-    //   });
-    // });
+    document.querySelector("#text").style.opacity = "0";
+    document.querySelector("#author").style.opacity = "0";
 
-    this.setState({
-      text: "If you have knowledge, let others light their candles in it.",
-      author: "Margaret Fuller",
+    axios.get(quoteURL).then((response) => {
+      this.setState({
+        text: response.data.content,
+        author: response.data.author,
+      });
     });
+
+    setTimeout(() => {
+      document.querySelector("#text").style.opacity = "1";
+      document.querySelector("#author").style.opacity = "1";
+
+      let color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+      document.querySelector(
+        "#quote-box"
+      ).style.boxShadow = `0 0 20px 0 ${color}`;
+      document.querySelector(
+        "#new-quote"
+      ).style.boxShadow = `0 0 1px 1px ${color}`;
+      document.querySelector("#text > svg").style.color = color;
+      document.querySelector("#tweet-quote").style.backgroundColor = color;
+    }, 500);
   }
 
   render() {
     return (
       <div id="quote-box">
         <div id="text">
-          <i className="fa fa-quote-left"></i>
+          <FontAwesomeIcon icon={faQuoteLeft} />
           <p>{this.state.text}</p>
         </div>
         <p id="author">{this.state.author}</p>
-        <a id="tweet-quote" href="twitter.com/intent/tweet">
-          tweet
+        <a
+          id="tweet-quote"
+          href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+            '"' + this.state.text + '"'
+          )} by ${encodeURIComponent(this.state.author)}`}
+        >
+          <i className={"fa fa-twitter"}></i>
         </a>
         <button id="new-quote" onClick={this.populateNewQuote}>
-          new quote
+          <FontAwesomeIcon icon={faArrowsRotate} />
+          {/* new quote */}
         </button>
       </div>
     );
